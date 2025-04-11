eb6ff63ea64db50b6d325db174d9fc57cb60c210
FedeCero505 <fcerocchi@frba.utn.edu.ar>
2025-04-11 20:14:23 -0300

    Update main.c
    Arreglado

--- cpu/src/main.c
+++ cpu/src/main.c
@@ -4,49 +4,42 @@
 #include<sys/socket.h>
 #include<netdb.h>
 
-struct PCB
-{
-    int PID;
-    int PC;
-    
-};
+
+t_log *log_cpu = NULL;
+t_config* cpu_conf = NULL;
 
 t_config* iniciar_config()
 {
-	t_config* nuevo_config = config_create("kernel.conf");
+	t_config* nuevo_config = config_create("cpu.conf");
 	return nuevo_config;
 }
 
-t_log *log_kernel = NULL;
 
-int main(int argc, char* argv[]) {
-    //saludar("kernel");
+int main(int argc, char* argv[]) 
+{
+    log_cpu = log_create("cpu.log", "cpu", true, LOG_LEVEL_INFO);
+    cpu_conf = iniciar_config();  
+    //inicia conexion con Kernel dispatch
+    char* ip_kernel_dispatch = config_get_string_value(cpu_conf, "IP_KERNEL");
+    char* puerto_kernel_disptach = config_get_string_value(cpu_conf, "PUERTO_KERNEL_DISPATCH");
+    int conexion_kernel_dispatch = iniciar_conexion(ip_kernel_dispatch, puerto_kernel_disptach);
 
-    t_config* config_kernel = iniciar_config("kernel");
+    log_info(log_cpu, "conexion creada?");
+    //Inicia conexion con la memoria
+    //int conexion_memoria = iniciar_conexion("8002");
+    //inicia conexion con Kernel interrput
+    //int conexion_kernel_interrput = iniciar_conexion("8003");
 
-    log_kernel = log_create("kernel.log", "kernel", true, LOG_LEVEL_INFO);
-    /*
-    char *nombreArchivo = NULL;
-    char *tamanioProceso = NULL;
+}
 
-    if (argc < 3)
-    {
-        log_info(log_kernel, "Error, Parametros INvalidos");
-        return 1;
-    }
-    nombreArchivo = argv[1];
-    tamanioProceso = argv[2];
-
-    log_info(log_kernel, "Archivo: %s, tamanio: %s",nombreArchivo, tamanioProceso);
-    */
-    char* puerto_escucha_dispatch = config_get_string_value(config_kernel, "PUERTO_ESCUCHA_DISPATCH");
-    int socket_dispatch = iniciar_modulo(puerto_escucha_dispatch);
-
-
-    int cpu_conectada = establecer_conexion(socket_dispatch);
-    
-    log_info(log_kernel, "Funco quizas?");
+int recibir_procesos(int conexion)
+{
+    //IMplementar escucha del CPU para recibir los PID y el PC --> una vez los reciba
 
     return 0;
 }
 
+int procesamiento(int pid, int pc, int conexion_memoria)
+{
+    //procesamiento en proceso
+}
