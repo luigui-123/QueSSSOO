3c98011de61c074faf4d09f003b37d1da568f87b
FedeCero505 <fcerocchi@frba.utn.edu.ar>
2025-04-11 20:14:43 -0300

    Update main.c
    Kernel Arreglado

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -8,21 +8,24 @@ struct PCB
 {
     int PID;
     int PC;
+    
 };
 
+t_config* iniciar_config()
+{
+	t_config* nuevo_config = config_create("kernel.conf");
+	return nuevo_config;
+}
 
-t_log *log_kernel;
+t_log *log_kernel = NULL;
 
 int main(int argc, char* argv[]) {
     //saludar("kernel");
 
-    int conexion_cpu_ejecutar;
-    int conexion_cpu_interrupciones;
-
-    t_config* config = iniciar_config("kernel");
+    t_config* config_kernel = iniciar_config("kernel");
 
     log_kernel = log_create("kernel.log", "kernel", true, LOG_LEVEL_INFO);
-
+    /*
     char *nombreArchivo = NULL;
     char *tamanioProceso = NULL;
 
@@ -35,6 +38,15 @@ int main(int argc, char* argv[]) {
     tamanioProceso = argv[2];
 
     log_info(log_kernel, "Archivo: %s, tamanio: %s",nombreArchivo, tamanioProceso);
+    */
+    char* puerto_escucha_dispatch = config_get_string_value(config_kernel, "PUERTO_ESCUCHA_DISPATCH");
+    int socket_dispatch = iniciar_modulo(puerto_escucha_dispatch);
+
+
+    int cpu_conectada = establecer_conexion(socket_dispatch);
+    
+    log_info(log_kernel, "Funco quizas?");
 
     return 0;
 }
+
