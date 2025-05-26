15b5caf00b6a142b8306edd150af9986dc81c30a
AgustinHutter <agustinhutter@gmail.com>
2025-05-26 02:02:23 +0000

    Malloc que imprime basura

--- memoria/src/main.c
+++ memoria/src/main.c
@@ -61,6 +61,13 @@ struct pcb
     int cantEscrituras;
 };
 
+struct tarea
+{
+    int id, num1, tam_texto, num2;
+    char* texto;
+};
+
+
 // Funciones que funcionan
 
 void iniciar_config()
@@ -81,12 +88,12 @@ void iniciar_config()
     DUMP_PATH= config_get_string_value(nuevo_conf, "DUMP_PATH");
     DIR_PSEUDOCODIGO=config_get_string_value(nuevo_conf, "PATH_PSEUDOCODIGO");
 
-    lista_procesos =list_create();
+    //lista_procesos =list_create();
 
     return;
 }
 
-void* gestion_conexiones(){
+/*void* gestion_conexiones(){
     // Crea socket y espera
     int socket_escucha = iniciar_modulo(PUERTO_ESCUCHA, log_memo);
     while(1){
@@ -100,26 +107,15 @@ void* gestion_conexiones(){
     }
     close(socket_escucha);
     return;
-}
+}*/
 
 
 
 // FUNCIONES PARA VER
 
-void* guardar_SWAP(void* proceso){
-    // ESCRIBIR EN EL FINAL DEL ARCHIVO Y USAR SEMAFORO
-    return;
-}
-
-void* leer_SWAP(){
-    struct pcb p;
-    // cargar EN EL FINAL DEL ARCHIVO
-    return;
-}
-
 void* ingresar_conexion(void* conexion)
 {
-    
+    /*
     // Transformamos la conexion a INT
     int aux=(int*) conexion;
 
@@ -214,35 +210,116 @@ void* ingresar_conexion(void* conexion)
         }
         free (titulo);
         break;
-    }
+    }*/
 
     // Permitimos el paso de la siguiente instruccion
     sem_post(consultar_memoria);
     return;
 }
 
+int str_to_int(char * txt, int ac){
+    int num=0,i=ac;
+
+    while (txt[i]!=' ')
+    {
+        num=num*10;
+        num+=txt[i]-'0';
+        printf("%d",num);
+        i++;
+    }
+
+    return num;
+}
+
 void leer_pseudo(){
     if( (FILE_PSEUDOCODIGO=fopen(DIR_PSEUDOCODIGO,"r")) !=NULL){ // VER COMO ENVIAR MENOS DE 255
-        char cont [255];
-        while (fgets(cont,255,FILE_PSEUDOCODIGO)){
-            char * guarda = malloc(sizeof(char) * 256);
-            guarda=strcat(cont,"\0");
+        char cont [100];
+        //int p,s;
+        while (fgets(cont,100,FILE_PSEUDOCODIGO)){
+            /*struct tarea tarea_act;
+            
+            p=cont[0]-'0';
+            s=cont[1]-'0';
+            printf("%d",p);
+            switch (p)
+            {
+            case 30: // NOOP
+                tarea_act.id= 0;
+                break;
+            case 39: // WRITE
+                tarea_act.id= 1;
+                tarea_act.num1 = str_to_int(cont,6);
+                printf("%d",tarea_act.num1);
+                break;
+            case 34: // READ
+                
+                break;
+            case 23: // GOTO
+               
+                break;
+            case 25:
+                if (s== 31){}// IO
+                else{}                   // INIT
+                break;
+            case 20: // DUMP MEMORY
+                
+                break;
+            case 21: // EXIT
+               
+                break;
+            default:
+                break;
+            }*/
+            printf("%s\n",cont);  // BIEN
+            char * guarda = malloc(sizeof(char) * 101);
+            memcpy(guarda,cont,sizeof(char)*100); //memcpy
+            memcpy(guarda+100,"\0",sizeof(char));
+            printf("copia %s\n",guarda);
+
+
+
             list_add(lista_procesos,guarda);
         }
     }
     fclose(FILE_PSEUDOCODIGO);
     return;
 }
+//mmap
 
 void enviar_lista(){
-    while (!list_is_empty(lista_procesos) )
+    for (int i = 0; i < list_size(lista_procesos); i++)
     {
-        printf(list_remove(lista_procesos,0)); // NO LO MUESTRA POR LA CONSOLA 
+        char mensaje[101];
+        char *posi=list_get(lista_procesos,i);
+        for (int j=0; j < 101; j++)
+        {
+           mensaje[j]=(posi+j);
+        }
+        
+        printf("%s\n",mensaje);  // IMPRIME BASURA
     }
+    
+    return;
+}
+
+void* gestion_conexiones(){
+    // Crea socket y espera
+    int socket_escucha = iniciar_modulo(PUERTO_ESCUCHA, log_memo);
+    while(1){
+        
+        // Recibe un cliente y crea un hilo personalizado para la conexión
+        socket_conectado = establecer_conexion(socket_escucha, log_memo);
+        pthread_t manejo_servidor;
+        pthread_create(&manejo_servidor,NULL,ingresar_conexion,(void*) socket_conectado);
+        pthread_detach(manejo_servidor);
+        
+    }
+    close(socket_escucha);
     return;
 }
 
 int main(int argc, char* argv[]) {
+
     // Crea un hilo que carga las variables globales. El sistema debe esperar que termine
     consultar_memoria=sem_open("SEM_MOD_MEMO", O_CREAT | O_EXCL, S_IRUSR | S_IRUSR, 0); 
 
@@ -255,10 +332,10 @@ int main(int argc, char* argv[]) {
     log_memo = log_create("memoria.log", "memoria", false, LOG_LEVEL);
 
 
-    leer_pseudo();
-    //enviar_lista();
-    log_info(log_info,("tiene %d",list_size(lista_procesos)));   // NO TIENE NADA?
-    log_info(log_memo,list_remove(lista_procesos,0));
+    leer_pseudo(lista_procesos);
+    enviar_lista(lista_procesos);
+    //log_info(log_info,("tiene %d",list_size(lista_procesos)));   // NO TIENE NADA?
+    //log_info(log_memo,list_remove(lista_procesos,0));
 
 
 
