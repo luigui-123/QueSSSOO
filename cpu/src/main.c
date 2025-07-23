041277d87895f0d3059762b8e2778d61e09399b8
AgustinHutter <132310340+AgustinHutter@users.noreply.github.com>
2025-07-23 19:12:11 -0300

    Ver tlc y cache

--- cpu/src/main.c
+++ cpu/src/main.c
@@ -334,7 +334,7 @@ void actualizar_cache(Pagina *cache, Pagina *pagina, cpuinfo *proceso, TLBEntrad
     int indice;
     if (!strcmp(reemplazo_cache, "CLOCK"))
     {
-        if ((indice = buscar_cache(cache, pagina->numero_pagina)) + 1)
+        if ((indice = buscar_cache(cache, pagina->numero_pagina)) + 1) // RETORNA INDICE DE PAG EN CACHE
         {
             cache[indice].referencia_bit = 1;
             reemplazado = true;
@@ -349,6 +349,7 @@ void actualizar_cache(Pagina *cache, Pagina *pagina, cpuinfo *proceso, TLBEntrad
                 {
                     cambio_cache->contenido = cache[puntero].contenido;
                     direccion_logica = cache[puntero].numero_pagina * tam_pagina;
+                    
                     cambio_cache->direccion_fisica = traducir_direccion(direccion_logica, proceso, tlb);
                     
                     agregar_a_paquete(paquete,&cambio_cache->tipo,sizeof(int));
@@ -610,7 +611,12 @@ void decodear_y_ejecutar_instruccion(char *instruccion, cpuinfo *proceso, bool *
             agregar_a_paquete(paquete, dato, longitud * sizeof(char));
             enviar_paquete(paquete, conexion_memoria);
             eliminar_paquete(paquete);
-            recibir_mensaje(conexion_memoria); // Recibo el OK de memoria
+            char* chivo =recibir_mensaje(conexion_memoria); // Recibo el OK de memoria
+            //strcat(chivo,"\0");
+            if(strcmp(chivo,"OK")){
+                log_info(log_cpu,"no se escribio en memoria");
+                abort();
+            }
             log_info(log_cpu, "PID: %d - Acción: ESCRIBIR - Dirección Física: %d - Valor: %s", proceso->pid, dir_fisica, dato);
             proceso->pc = proceso->pc + 1;
 
@@ -621,7 +627,7 @@ void decodear_y_ejecutar_instruccion(char *instruccion, cpuinfo *proceso, bool *
                 pagina_cache->modificado = 0;
                 pagina_cache->referencia_bit = 1;
                 memoriainfo *pagina = malloc(sizeof(memoriainfo));
-                pagina->tipo = 3;
+                pagina->tipo = 3; // LEER
                 pagina->pid = proceso->pid;
                 pagina->direccion = dir_fisica - desplazamiento;
                 t_paquete *paquete_pagina = crear_paquete();
@@ -809,11 +815,11 @@ void decodear_y_ejecutar_instruccion(char *instruccion, cpuinfo *proceso, bool *
     else if (!strcmp(comando, "NOOP"))
     {
         proceso->pc++;
-        log_trace(log_cpu, "NOOP subiendo?: %d", proceso->pc);
+        log_info(log_cpu, "NOOP subiendo?: %d", proceso->pc);
     }
     else
     {
-        log_trace(log_cpu, "Instruccion desconocida: %s", comando);
+        log_info(log_cpu, "Instruccion desconocida: %s", comando);
         for (int i=0; i<strlen(comando); i++) log_trace(log_cpu, "ASCII: %c", comando[i]);
     }
 
@@ -834,7 +840,7 @@ int main(int argc, char *argv[])
 
     log_cpu = log_create(nombre_log_cpu, "cpu", true, LOG_LEVEL_INFO);
 
-    char *path_config = "/home/utnso/tp-2025-1c-RompeComputadoras/cpu/cpu.conf";
+    char *path_config = "/home/utnso/Desktop/tp-2025-1c-RompeComputadoras/cpu/cpu.conf";
 //argv[1]
     iniciar_config(path_config);
 
